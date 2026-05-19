506ec4a0cf104e1b98e2e3a2efd7ff0a67044267
Surajit Rana <surajitrana.swe@gmail.com>
2026-05-19 23:32:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (68.73%) - LeetSync

---
+++ TreesPattern/Traversal/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * Approach : Using DFS Inorder Traversal (Left Node Right) Approach
+     *
+     * TC : O(n)
+     * SC : O(h) (O(n) in worst case in case of skewed tree)
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfsTree(root, result);
+        return result;
+    }
+
+    /**
+     * Using DFS Inorder Traversal (Left Node Right) Approach
+     *
+     * TC : O(n)
+     * SC : O(h)
+     */
+    private void dfsTree(TreeNode node, List<Integer> result) {
+        // Base Case
+        if (node == null) {
+            return;
+        }
+        // Recursion Calls
+        dfsTree(node.left, result);
+        result.add(node.val);
+        dfsTree(node.right, result);
+    }
+}
