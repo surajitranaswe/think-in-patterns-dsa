0475067f0ee65647351c67ab1693152b1a7906e8
Surajit Rana <surajitrana.swe@gmail.com>
2026-05-24 02:27:25 +0530

    Time: 6 ms (99.05%) | Memory: 46.9 MB (53.08%) - LeetSync

--- StringsMatchingPattern/MatchingAlgorithms/459-repeated-substring-pattern/repeated-substring-pattern.java
+++ StringsMatchingPattern/MatchingAlgorithms/459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -2,48 +2,32 @@ class Solution {
     /**
      * Approach : Using KMP Algorithm Approach
      *
-     * TC : O(n) + O(n) ~ O(n)
-     * SC : O(n) + O(n) ~ O(n)
+     * TC : O(n)
+     * SC : O(n)
      */
     public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
-        Set<Integer> lpsSet = computeLPSArray(s, n); // TC : O(n), SC : O(n)
-        StringBuilder sb = new StringBuilder(); // SC : O(n)
-        for (Integer lps : lpsSet) {            // TC : O(k)
-            String sub = s.substring(0, lps);   // SC : O(k)
-            sb.setLength(0);
-            int times = n / lps;
-            int j = 0;
-            while (j < times) { // TC : O(n / k)
-                sb.append(sub);
-                j++;
-            }
-            if (s.equals(sb.toString())) {
-                return true;
-            }
-        }
-        return false;
+        int[] lps = new int[n]; // SC : O(n)
+        computeLPSArray(s, n, lps);   // TC : O(n), SC : O(1)
+        // length of prefix which is also a suffix ending at index (n - 1)
+        int len = lps[n - 1];
+        return len > 0 && n % (n - len) == 0;
     }
 
     /**
      * Using KMP Algorithm for LPS Array Approach
      *
      * TC : O(n)
-     * SC : O(n) + O(k) ~ O(n)
+     * SC : O(1)
      */
-    private Set<Integer> computeLPSArray(String s, int n) {
-        int[] lps = new int[n]; // SC : O(n)
-        Set<Integer> lpsSet = new HashSet<>(); // SC : O(k)
+    private void computeLPSArray(String s, int n, int[] lps) {
         lps[0] = 0;
         int len = 0;
         int i = 1;
-        while (i < n) { // TC : O(n)
+        while (i < n) {
             if (s.charAt(len) == s.charAt(i)) {
                 len++;
                 lps[i] = len;
-                if (lps[i] > 0) {
-                    lpsSet.add(lps[i]);
-                }
                 i++;
             } else {
                 if (len != 0) {
@@ -53,6 +37,5 @@ class Solution {
                 }
             }
         }
-        return lpsSet;
     }
 }
