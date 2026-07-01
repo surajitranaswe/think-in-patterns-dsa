646a4a499751e452a2f7ded5b855b298eeaf72fa
Surajit Rana <surajitrana.swe@gmail.com>
2026-07-01 23:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (55.92%) - LeetSync

---
+++ Dynamic_Programming/1D_DP/Medium/198-house-robber/house-robber.java
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * Approach I : Using Tabulation (Bottom-Up) Approach
+	 *
+	 * TC : O(n)
+	 * SC : O(n) 
+	 */
+    public int rob(int[] nums) {
+        int n = nums.length;
+		int[] dp = new int[n]; // SC : O(n)
+		/**
+		 * here dp[i] denotes the maximum money
+		 * that can be looted till index 'i'
+		 *
+		 * dp[0] = nums[0];
+		 * dp[1] = Max(dp[0], nums[1]);
+		 * as we cannot pick two consecutive indices
+		 * dp[2] = Math.max(nums[2] + dp[0], dp[1])
+		 */
+		dp[0] = nums[0];
+		for (int i = 1; i < n; i++) { // TC : O(n)
+			dp[i] = Math.max(
+				nums[i] + (i > 1 ? dp[i - 2] : 0),
+				dp[i - 1]
+			);
+		}
+		return dp[n - 1];
+    }
+}
