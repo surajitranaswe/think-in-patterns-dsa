135165d8ef3aac725047cfca3e7599d7d6c4fa68
Surajit Rana <surajitrana.swe@gmail.com>
2026-07-05 00:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (11.42%) - LeetSync

--- Dynamic_Programming/Grid_DP/Medium/63-unique-paths-ii/unique-paths-ii.java
+++ Dynamic_Programming/Grid_DP/Medium/63-unique-paths-ii/unique-paths-ii.java
@@ -1,11 +1,48 @@
 class Solution {
     /**
+     * Approach II : Using Space Optimization (Optimized DP) Approach
+     *
+     * TC : O(m x n)
+     * SC : O(n) + O(n) ~ O(n)
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] prev = new int[n];   // SC : O(n)
+        prev[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int j = 1; j < n; j++) { // TC : O(n)
+            if (obstacleGrid[0][j] == 1) {
+                prev[j] = 0;
+            } else {
+                prev[j] = prev[j - 1];
+            }
+        }
+        for (int i = 1; i < m; i++) {     // TC : O(m)
+            int[] current = new int[n];   // SC : O(n)
+            if (obstacleGrid[i][0] == 1) {
+                current[0] = 0;
+            } else {
+                current[0] = prev[0];
+            }
+            for (int j = 1; j < n; j++) { // TC : O(n)
+                if (obstacleGrid[i][j] == 1) {
+                    current[j] = 0;
+                } else {
+                    current[j] = current[j - 1] + prev[j];
+                }
+            }
+            prev = current.clone();
+        }
+        return prev[n - 1];
+    }
+
+    /**
      * Approach I : Using Tabulation (Bottom-Up) Approach
      *
      * TC : O(m x n)
      * SC : O(m x n)
      */
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public int uniquePathsWithObstaclesTabulation(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[m][n];   // SC : O(m x n)
