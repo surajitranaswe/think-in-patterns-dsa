ad950ec12514f045538bc40e5f1259dcf54e3a58
Surajit Rana <surajitrana.swe@gmail.com>
2026-07-08 00:38:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.69%) - LeetSync

--- Dynamic_Programming/1D_DP/Easy/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ Dynamic_Programming/1D_DP/Easy/1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,11 +1,33 @@
 class Solution {
     /**
+     * Approach II : Using Optimized DP Approach
+     *
+     * TC : O(n)
+     * SC : O(1)
+     */
+    public int tribonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int prev2 = 0;
+        int prev1 = 1;
+        int prev = 1;
+        for (int i = 3; i <= n; i++) { // TC : O(n)
+            int current = prev + prev1 + prev2;
+            prev2 = prev1;
+            prev1 = prev;
+            prev = current;
+        }
+        return prev;
+    }
+
+    /**
      * Approach I : Using Top-Down DP Approach
      *
      * TC : O(n)
      * SC : O(n)
      */
-    public int tribonacci(int n) {
+    public int tribonacciTabulation(int n) {
         if (n <= 1) {
             return n;
         }
