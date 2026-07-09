24221545c06bad9c8a2fbd3e1c9a856544eb64d7
Surajit Rana <surajitrana.swe@gmail.com>
2026-07-10 00:18:20 +0530

    Time: 50 ms (47.19%) | Memory: 46.7 MB (66.68%) - LeetSync

--- Dynamic_Programming/KnapsackDP/Medium/416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ Dynamic_Programming/KnapsackDP/Medium/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,11 +1,54 @@
 class Solution {
     /**
+     * Approach II : Using Optimized DP Approach
+     *
+     * TC : O(n) + O(n x k) ~ O(n x k)
+     * SC : O(k)
+     */
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            sum += nums[i];
+        }
+        if ((sum & 1) != 0) {
+            /**
+             * if sum is odd then we cannot partition
+             * 'nums' into two subsets of equal sum
+             */
+            return false;
+        }
+        sum = sum / 2;
+        /**
+         * now the problem is converted to return if
+         * array 'nums' can have a subset sum target = sum
+         */
+        boolean[] prev = new boolean[sum + 1]; // SC : O(k)
+        prev[0] = true;
+        for (int i = 1; i <= n; i++) {       // TC : O(n)
+            boolean[] current = new boolean[sum + 1]; // SC : O(k)
+            current[0] = true;
+            for (int j = 1; j <= sum; j++) { // TC : O(k)
+                // skip
+                boolean skip = prev[j];
+                boolean pick = false;
+                if (nums[i - 1] <= j) {
+                    pick = prev[j - nums[i - 1]];
+                }
+                current[j] = skip || pick;
+            }
+            prev = current;
+        }
+        return prev[sum];
+    }
+
+    /**
      * Approach I : Using Bottom-Up DP Approach
      *
      * TC : O(n) + O(n x k) ~ O(n x k)
      * SC : O(n x k)
      */
-    public boolean canPartition(int[] nums) {
+    public boolean canPartitionTabulation(int[] nums) {
         int n = nums.length;
         int sum = 0;
         for (int i = 0; i < n; i++) { // TC : O(n)
