4a820bce2a8f2e3fba7ab627d087d5048948767d
Surajit Rana <surajitrana.swe@gmail.com>
2026-07-10 00:14:34 +0530

    Time: 52 ms (42.00%) | Memory: 47.5 MB (53.03%) - LeetSync

---
+++ Dynamic_Programming/KnapsackDP/Medium/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * Approach I : Using Bottom-Up DP Approach
+     *
+     * TC : O(n) + O(n x k) ~ O(n x k)
+     * SC : O(n x k)
+     */
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) { // TC : O(n)
+            sum += nums[i];
+        }
+        if ((sum & 1) != 0) {
+            /**
+             * if sum is odd then we cannot partition
+             * 'nums' into two subsets of equal sum
+             */
+            return false;
+        }
+        sum = sum / 2;
+        /**
+         * now the problem is converted to return if
+         * array 'nums' can have a subset sum target = sum
+         */
+        boolean[][] dp = new boolean[n + 1][sum + 1]; // SC : O(n x k)
+        /**
+         * dp[i][j] denotes if it is possible to form a 
+         * sum 'j' if we consider elements from [0 to (i - 1)]
+         */
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {       // TC : O(n)
+            for (int j = 1; j <= sum; j++) { // TC : O(k)
+                // skip
+                boolean skip = dp[i - 1][j];
+                boolean pick = false;
+                if (nums[i - 1] <= j) {
+                    pick = dp[i - 1][j - nums[i - 1]];
+                }
+                dp[i][j] = skip || pick;
+            }
+        }
+        return dp[n][sum];
+    }
+}
